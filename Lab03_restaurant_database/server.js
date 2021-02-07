30910cbcbef9c05bcd80e297a4b65e1280adab1f
linminca <min-lin@hotmail.com>
2021-02-07 01:38:00 -0500

    add lab3

---
+++ Lab03_restaurant_database/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const employeeRouter = require('./routes/EmployeeRoutes.js');
+const restaurantRouter = require('./routes/RestaurantRoutes.js');
+
+
+const app = express();
+app.use(express.json()); // Make sure it comes back as json
+
+//TODO - Replace you Connection String here
+mongoose.connect('mongodb+srv://dbAdmin:C$MYGy4dss@cluster0.shjls.mongodb.net/gbc-full-stack?retryWrites=true&w=majority', {
+  useCreateIndex: true,  
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// app.use(employeeRouter);
+app.use(restaurantRouter);
+
+app.listen(3000, () => { console.log('Server is running...') });
